b209d93dfb4bf447614f1362507eabcd45e39c41
Pierre TUDELA <pierre.tudela@eurotitrisation.fr>
2023-01-24 16:03:15 +0100

    removed unessary usngs

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -1,15 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Excel
 {
